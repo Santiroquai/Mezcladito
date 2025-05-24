11a5fc94733a4f53be754a11b34204ef25a25d94
mi primer commit: instalacion de vue 3 <>
2025-05-24 11:29:34 -0300

    se cambio el bg de absolute a relarive y se agrego un z-index. Para solucionar el error de imagen de fondo con movimiente en moviles

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@ import TeamGenerator from './components/TeamGenerator.vue'
 </script>
 
 <template>
-  <div class="bg-green-600 relative min-h-screen overflow-x-hidden">
-    <div class="absolute inset-0 custom-bg w-full blur-sm"></div>
-    
-    <div class="relative p-2 sm:p-4 md:p-6">
+  <div class="relative min-h-screen overflow-x-hidden">
+    <!-- Fondo fijo con blur y detrás del contenido -->
+    <div class="fixed inset-0 w-full h-full custom-bg blur-sm z-[-1]"></div>
+
+    <!-- Contenido encima del fondo -->
+    <div class="relative z-10 p-2 sm:p-4 md:p-6">
       <div class="max-w-screen-xl mx-auto">
         <header class="mb-3 sm:mb-4 mt-2 sm:mt-3 md:mb-8 text-center">
           <h1 class="text-xl sm:text-2xl md:text-3xl font-bold text-white mb-1 sm:mb-2">⚽ Mezcladito ⚽</h1>

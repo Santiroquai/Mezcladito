0c48fd7ec62d85df845114a1608458f2db0d27a6
mi primer commit: instalacion de vue 3 <>
2025-05-26 11:16:05 -0300

    padding mas ancho

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import TeamGenerator from './components/TeamGenerator.vue'
           <h1 class="text-xl sm:text-2xl md:text-3xl font-bold text-white mb-1 sm:mb-2">⚽ Mezcladito ⚽</h1>
         </header>
 
-        <div class="w-full max-w-full px-1 sm:px-2 md:max-w-2xl mx-auto grid md:grid-cols-1 gap-3 sm:gap-4 md:gap-8">
+        <div class="w-full max-w-full px-2 sm:px-2 md:max-w-2xl mx-auto grid md:grid-cols-1 gap-3 sm:gap-4 md:gap-8">
           <PlayerList />
           <SelectedPlayers />
           <TeamGenerator /> 
